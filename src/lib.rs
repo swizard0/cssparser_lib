e3f3889b0b8f345ec58d69f0783927c6dac0ce66
Alexey Voznyuk <me@swizard.info>
2016-12-20 23:33:51 +0300

    some more stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,24 @@
+extern crate libc;
 extern crate cssparser;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+use libc::{c_int, c_char, size_t};
+use cssparser::{Parser, RuleListParser, decode_stylesheet_bytes};
+
+const C_RES_OK: c_int = 0;
+const C_RES_ERR_INVALID_PARAMETER: c_int = 1;
+
+#[no_mangle]
+pub extern fn parse_document(css: *const c_char, css_size: size_t) -> c_int {
+    if css.is_null() {
+        return C_RES_ERR_INVALID_PARAMETER;
     }
+
+    let css_slice = unsafe { std::slice::from_raw_parts(css as *const u8, css_size as usize) };
+    let (css_unicode, encoding) =
+        decode_stylesheet_bytes(css_slice, None, None);
+    let input = &mut Parser::new(&css_unicode);
+
+    //let rules = RuleListParser::new_for_stylesheet(input, ?).collect::<Vec<_>>();
+
+    C_RES_OK
 }
